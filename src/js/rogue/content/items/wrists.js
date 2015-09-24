5f178c0fa70a25cfae94e9fecc1391f9ebc20d53
seiyria <kyle@seiyria.com>
2015-09-24 12:26:57 -0500

    added gloves

--- src/js/rogue/content/items/wrists.js
+++ src/js/rogue/content/items/wrists.js
@@ -1,6 +1,34 @@
 
 import { Wrist } from '../../definitions/equipment';
+import { AC } from '../../lib/default-calculations';
+import Materials from '../../constants/materials';
 
-export class Leather extends Wrist {
-  static get rarity() { return 100; }
+export class LeatherGloves extends Wrist {
+  get material() { return Materials.Leather; }
+  static get rarity() { return 75; }
+  ac() {
+    return AC(this);
+  }
+}
+
+export class GauntletsOfDexterity extends Wrist {
+  get material() { return Materials.Leather; }
+  static get rarity() { return 10; }
+  ac() {
+    return AC(this);
+  }
+  dex() {
+    return this.buc * -2;
+  }
+}
+
+export class GauntletsOfStrength extends Wrist {
+  get material() { return Materials.Leather; }
+  static get rarity() { return 10; }
+  ac() {
+    return AC(this);
+  }
+  str() {
+    return this.buc * -2;
+  }
 }
