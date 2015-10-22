0e6db4bdf2d532f7c04680f26f7782de4f1de62c
seiyria <kyle@seiyria.com>
2015-10-22 12:17:28 -0500

    fix LizardMan -> Lizardman

--- src/js/rogue/content/races/_all.js
+++ src/js/rogue/content/races/_all.js
@@ -7,17 +7,17 @@ import Gnome from './gnome';
 import Human from './human';
 import Insect from './insect';
 import Jelly from './jelly';
-import LizardMan from './lizardman';
 import Manticore from './manticore';
 import Orc from './orc';
 import Psionic from './psionic';
 import Pudding from './pudding';
 import Rat from './rat';
 import Lizard from './lizard';
+import Lizardman from './lizardman';
 import Snake from './snake';
 import Spider from './spider';
 import Spore from './spore';
 
 export default { Canine, Demon, Elemental, Elf, Gnome,
-  Human, Insect, Jelly, Lizard, LizardMan, Manticore, Orc, Pudding, Psionic, Rat,
+  Human, Insect, Jelly, Lizard, Lizardman, Manticore, Orc, Pudding, Psionic, Rat,
   Snake, Spider, Spore };
