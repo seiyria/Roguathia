6903088b8422b3413f509dc7e0875d3b92c0767e
seiyria <kyle@seiyria.com>
2015-10-24 11:35:59 -0500

    making eslint cache better

--- gulp/lint.js
+++ gulp/lint.js
@@ -1,12 +1,33 @@
 
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
+const cache = require('gulp-cached');
+const gulpIf = require('gulp-if');
+const through2 = require('through2');
 
 const getPaths = require('./_common').getPaths;
 
+const isLinty = (file) => {
+  // check if a file has any errors or warnings
+  return file.eslint != null
+    && (file.eslint.warningCount > 0 || file.eslint.errorCount > 0);
+};
+
+const uncache =(cacheName) => {
+  // create a stream that removes files from cache
+  return through2.obj((file, enc, done) => {
+    if (cache.caches[cacheName]) {
+      delete cache.caches[cacheName][file.path];
+    }
+    done(null, file);
+  });
+}
+
 gulp.task('eslint', () => {
   return gulp.src(getPaths().js)
+    .pipe(cache('lint-cache'))
     .pipe(eslint({ useEslintrc: true, cache: true }))
     .pipe(eslint.format())
-    .pipe(eslint.failOnError());
+    .pipe(eslint.failOnError())
+    .pipe(gulpIf(isLinty, uncache('lint-cache')));
 });
