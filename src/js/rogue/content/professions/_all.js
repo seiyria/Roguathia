95f76c41d82c4a3e70e95d9140d374dc478d8659
Kyle Kemp <kyle@seiyria.com>
2015-11-25 09:48:38 -0600

    added barbarian

--- src/js/rogue/content/professions/_all.js
+++ src/js/rogue/content/professions/_all.js
@@ -1,9 +1,10 @@
 
 import Archaeologist from './archaeologist';
+import Barbarian from './barbarian';
 import Developer from './developer';
 import Monster from './monster';
 import Ranger from './ranger';
 import Tourist from './tourist';
 import Wizard from './wizard';
 
-export default { Archaeologist, Developer, Monster, Ranger, Tourist, Wizard };
+export default { Archaeologist, Barbarian, Developer, Monster, Ranger, Tourist, Wizard };
---
+++ src/js/rogue/content/professions/barbarian.js
@@ -0,0 +1,38 @@
+
+import Profession from '../../definitions/profession';
+
+import * as Foods from '../items/foods';
+import * as Weapons from '../items/_weapons';
+import * as Bodys from '../items/bodys';
+import * as Thresholds from '../../constants/skill-thresholds';
+import * as Traits from '../traits/_all';
+
+const barbarianConfig = {
+  hp  : '6d5',
+  mp  : '0d0',
+  str : '4d3',
+  con : '3d2',
+  int : '1d1',
+  dex : '2d2',
+  wis : '1d1',
+  cha : '1d1',
+  titles: ['Plunderex',, 'Pillager',,, 'Bandit',,, 'Brigand',,, 'Raider',,, 'Reaver',,, 'Slayer',,, 'Chiefterex',,, 'Conquerex'],
+  traits: [Traits.Stealth({ level: 15, req: 15 }), Traits.Haste({ level: 50, req: 7 }), Traits.PoisonResistance()],
+  skillCaps: { bash: Thresholds.Skilled, stab: Thresholds.Skilled, ranged: Thresholds.Basic, shot: Thresholds.Basic, slash: Thresholds.Skilled, unarmed: Thresholds.Master },
+  startingItems: [
+    { choices: { swordaxe: 1, axesword: 1 },
+      choicesInit: {
+        swordaxe: () => [new Weapons.Broadsword({ bucName: 'uncursed' }), new Weapons.Axe({ bucName: 'uncursed' })],
+        axesword: () => [new Weapons.BattleAxe({ bucName: 'uncursed' }), new Weapons.Shortsword({ bucName: 'uncursed' })]
+      }
+    },
+    { init: () => new Foods.Ration({ charges: '1d2', bucName: 'uncursed' }) },
+    { init: () => new Bodys.RingMail({ bucName: 'uncursed', startIdentified: true }) }
+  ]
+};
+
+export default class Barbarian extends Profession {
+  constructor() {
+    super(barbarianConfig);
+  }
+}
