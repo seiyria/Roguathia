ba65e2fee04c10370034b262457bf2831fc398a9
Kyle Kemp <kyle@seiyria.com>
2015-11-25 07:51:59 -0600

    fixing size of extra info tab

--- src/js/angular/controllers/Party.js
+++ src/js/angular/controllers/Party.js
@@ -4,7 +4,7 @@ import GameState from '../../rogue/init/gamestate';
 
 module.controller('Party', ($scope) => {
 
-  $scope.inventoryOffset = () => 40 + document.getElementsByClassName('player-block')[0].offsetHeight;
+  $scope.inventoryOffset = () => 60 + document.getElementsByClassName('player-block')[0].offsetHeight;
 
   GameState.on('redraw', () => {
     $scope.players = GameState.players;
