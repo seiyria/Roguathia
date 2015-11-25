66a383dc90a736512f0cc2cf4d6cf8723c4b0e9d
Kyle Kemp <kyle@seiyria.com>
2015-11-25 10:02:50 -0600

    added caveman

---
+++ src/js/rogue/content/professions/caveman.js
@@ -0,0 +1,35 @@
+
+import Profession from '../../definitions/profession';
+
+import * as Weapons from '../items/_weapons';
+import * as Bodys from '../items/bodys';
+import * as Projectiles from '../items/projectiles';
+import * as Thresholds from '../../constants/skill-thresholds';
+import * as Traits from '../traits/_all';
+
+const cavemanConfig = {
+  hp  : '6d5',
+  mp  : '0d0',
+  str : '6d2',
+  con : '1d2',
+  int : '1d1',
+  dex : '1d2',
+  wis : '1d1',
+  cha : '1d1',
+  titles: ['Troglodyte',, 'Aborigine',,, 'Wanderer',,, 'Vagrant',,, 'Wayfarer',,, 'Roamer',,, 'Nomad',,, 'Rover',,, 'Pioneer'],
+  traits: [Traits.Warning({ level: 15, req: 15 }), Traits.Haste({ level: 50, req: 7 })],
+  skillCaps: { bash: Thresholds.Expert, stab: Thresholds.Basic, ranged: Thresholds.Basic, shot: Thresholds.Basic, unarmed: Thresholds.Master },
+  startingItems: [
+    { init: () => new Weapons.Club({ bucName: 'uncursed', enchantment: 1 }) },
+    { init: () => new Weapons.Sling({ bucName: 'uncursed', enchantment: 2 }) },
+    { init: () => new Projectiles.FlintStone({ charges: '1d12 + 10', bucName: 'uncursed' }) },
+    { init: () => new Projectiles.Rock({ charges: '1d15 + 18', bucName: 'uncursed' }) },
+    { init: () => new Bodys.StuddedLeatherArmor({ bucName: 'uncursed', startIdentified: true }) }
+  ]
+};
+
+export default class Caveman extends Profession {
+  constructor() {
+    super(cavemanConfig);
+  }
+}
