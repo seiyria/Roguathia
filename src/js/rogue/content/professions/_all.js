e067910ac6670aac4f2c7a3e16510a75b2728d00
Kyle Kemp <kyle@seiyria.com>
2015-11-26 18:07:33 -0600

    added monk

--- src/js/rogue/content/professions/_all.js
+++ src/js/rogue/content/professions/_all.js
@@ -5,9 +5,10 @@ import Caveman from './caveman';
 import Developer from './developer';
 import Healer from './healer';
 import Knight from './knight';
+import Monk from './monk';
 import Monster from './monster';
 import Ranger from './ranger';
 import Tourist from './tourist';
 import Wizard from './wizard';
 
-export default { Archaeologist, Barbarian, Caveman, Developer, Healer, Knight, Monster, Ranger, Tourist, Wizard };
+export default { Archaeologist, Barbarian, Caveman, Developer, Healer, Knight, Monk, Monster, Ranger, Tourist, Wizard };
---
+++ src/js/rogue/content/professions/monk.js
@@ -0,0 +1,41 @@
+
+import Profession from '../../definitions/profession';
+
+import * as Foods from '../items/foods';
+import * as Potions from '../items/potions';
+import * as Bodys from '../items/bodys';
+import * as Wrists from '../items/wrists';
+import * as Thresholds from '../../constants/skill-thresholds';
+import * as Traits from '../traits/_all';
+
+const monkConfig = {
+  hp  : '3d5',
+  mp  : '0d0',
+  str : '4d2',
+  con : '2d2',
+  int : '1d2',
+  dex : '4d2',
+  wis : '1d2',
+  cha : '1d1',
+  titles: ['Candidate',, 'Novice',,, 'Initiate',,, 'Student of Stones',,, 'Student of Waters',,, 'Student of Metals',,, 'Student of Winds',,, 'Student of fire',,, 'Master'],
+  traits: [
+    Traits.PoisonResistance({ req: 3 }), Traits.Stealth({ level: 15, req: 5 }), Traits.Warning({ level: 10, req: 7 }),
+    Traits.Haste({ level: 100, req: 9 }), Traits.FireResistance({ req: 11 }), Traits.IceResistance({ req: 13 }),
+    Traits.ShockResistance({ req: 15 }), Traits.Infravision({ level: 3, req: 17 }), Traits.Protection({ level: 1, req: 19 })
+  ],
+  skillCaps: { bash: Thresholds.Basic, unarmed: Thresholds.Grandmaster, force: Thresholds.Basic },
+  startingItems: [
+    { init: () => new Bodys.Robe({ bucName: 'uncursed', enchantment: 2 }) },
+    { init: () => new Wrists.LeatherGloves({ bucName: 'uncursed', enchantment: 2, startIdentified: true }) },
+    { init: () => new Potions.Healing({ charges: '1d1', bucName: 'uncursed', startIdentified: true }) },
+    { init: () => new Foods.Ration({ charges: '1d3 + 3', bucName: 'uncursed' }) },
+    { init: () => new Foods.Apple({ charges: '1d5 + 5', bucName: 'uncursed' }) },
+    { init: () => new Foods.Carrot({ charges: '1d5 + 5', bucName: 'uncursed' }) }
+  ]
+};
+
+export default class Monk extends Profession {
+  constructor() {
+    super(monkConfig);
+  }
+}
